a34d0559767babc52d8562897a354afe61e2ec80
Aleksandar Kostadinov <akostadi@redhat.com>
2018-08-03 01:03:36 +0300

    first wild code dump to get script supported in JobProperty

--- src/main/java/jenkins/plugins/logstash/LogstashConsoleLogFilter.java
+++ src/main/java/jenkins/plugins/logstash/LogstashConsoleLogFilter.java
@@ -2,9 +2,12 @@ package jenkins.plugins.logstash;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.annotation.CheckForNull;
 
 import hudson.Extension;
 import hudson.console.ConsoleLogFilter;
@@ -12,18 +15,28 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Run;
 
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
+
 @Extension(ordinal = 1000)
 public class LogstashConsoleLogFilter extends ConsoleLogFilter implements Serializable
 {
 
   private static Logger LOGGER = Logger.getLogger(LogstashConsoleLogFilter.class.getName());
 
+  @CheckForNull
+  private SecureGroovyScript secureGroovyScript = null;
   private transient Run<?, ?> run;
   public LogstashConsoleLogFilter() {};
 
   public LogstashConsoleLogFilter(Run<?, ?> run)
   {
+    this(run, null);
+  }
+
+  public LogstashConsoleLogFilter(Run<?, ?> run, SecureGroovyScript script)
+  {
     this.run = run;
+    this.secureGroovyScript = script;
   }
   private static final long serialVersionUID = 1L;
 
@@ -41,7 +54,7 @@ public class LogstashConsoleLogFilter extends ConsoleLogFilter implements Serial
     {
       if (isLogstashEnabled(build))
       {
-        LogstashWriter logstash = getLogStashWriter(build, logger);
+        LogstashWriter logstash = getLogStashWriter(build, logger, null);
         return new LogstashOutputStream(logger, logstash);
       }
       else
@@ -51,7 +64,7 @@ public class LogstashConsoleLogFilter extends ConsoleLogFilter implements Serial
     }
     if (run != null)
     {
-      LogstashWriter logstash = getLogStashWriter(run, logger);
+      LogstashWriter logstash = getLogStashWriter(run, logger, secureGroovyScript);
       return new LogstashOutputStream(logger, logstash);
     }
     else
@@ -60,9 +73,13 @@ public class LogstashConsoleLogFilter extends ConsoleLogFilter implements Serial
     }
   }
 
-  LogstashWriter getLogStashWriter(Run<?, ?> build, OutputStream errorStream)
+  LogstashWriter getLogStashWriter(Run<?, ?> build, OutputStream errorStream, SecureGroovyScript script)
   {
-    return new LogstashWriter(build, errorStream, null, build.getCharset());
+    LogstashScriptProcessor processor = null;
+    if (secureGroovyScript != null) {
+      processor = new LogstashScriptProcessor(secureGroovyScript, new OutputStreamWriter(errorStream, build.getCharset()));
+    }
+    return new LogstashWriter(build, errorStream, null, build.getCharset(), processor);
   }
 
   private boolean isLogstashEnabled(Run<?, ?> build)
--- src/test/java/jenkins/plugins/logstash/LogstashConsoloLogFilterTest.java
+++ src/test/java/jenkins/plugins/logstash/LogstashConsoloLogFilterTest.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import jenkins.plugins.logstash.persistence.BuildData;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
 
 import org.junit.After;
 import org.junit.Before;
@@ -43,7 +44,7 @@ public class LogstashConsoloLogFilterTest {
     }
 
     @Override
-    LogstashWriter getLogStashWriter(Run<?, ?> build, OutputStream errorStream) {
+    LogstashWriter getLogStashWriter(Run<?, ?> build, OutputStream errorStream, SecureGroovyScript script) {
       // Simulate bad Writer
       if(writer.isConnectionBroken()) {
         try {
