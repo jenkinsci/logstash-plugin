e66ddf894e39244a39e2e77f564747cfe037aedc
ziv <ziv4545@gmail.com>
2021-02-08 21:12:50 +0200

    fix logstashsendstep file lint

--- src/main/java/jenkins/plugins/logstash/pipeline/LogstashSendStep.java
+++ src/main/java/jenkins/plugins/logstash/pipeline/LogstashSendStep.java
@@ -1,7 +1,6 @@
 package jenkins.plugins.logstash.pipeline;
 
 import java.io.PrintStream;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,35 +20,41 @@ import jenkins.plugins.logstash.LogstashWriter;
 import jenkins.plugins.logstash.Messages;
 
 /**
- * Sends the tail of the log in a single event to a logstash indexer. Pipeline
- * counterpart of the LogstashNotifier.
+ * Sends the tail of the log in a single event to a logstash indexer.
+ * Pipeline counterpart of the LogstashNotifier.
  */
-public class LogstashSendStep extends Step {
+public class LogstashSendStep extends Step
+{
 
   private int maxLines;
   private boolean failBuild;
 
   @DataBoundConstructor
-  public LogstashSendStep(int maxLines, boolean failBuild) {
+  public LogstashSendStep(int maxLines, boolean failBuild)
+  {
     this.maxLines = maxLines;
     this.failBuild = failBuild;
   }
 
-  public int getMaxLines() {
+  public int getMaxLines()
+  {
     return maxLines;
   }
 
-  public boolean isFailBuild() {
+  public boolean isFailBuild()
+  {
     return failBuild;
   }
 
   @Override
-  public StepExecution start(StepContext context) throws Exception {
+  public StepExecution start(StepContext context) throws Exception
+  {
     return new Execution(context, maxLines, failBuild);
   }
 
-  @SuppressFBWarnings(value = "SE_TRANSIENT_FIELD_NOT_RESTORED", justification = "Only used when starting.")
-  private static class Execution extends SynchronousNonBlockingStepExecution<Void> {
+  @SuppressFBWarnings(value="SE_TRANSIENT_FIELD_NOT_RESTORED", justification="Only used when starting.")
+  private static class Execution extends SynchronousNonBlockingStepExecution<Void>
+  {
 
     private static final long serialVersionUID = 1L;
 
@@ -85,25 +90,28 @@ public class LogstashSendStep extends Step {
       }
       return null;
     }
-
   }
 
   @Extension
-  public static class DescriptorImpl extends StepDescriptor {
+  public static class DescriptorImpl extends StepDescriptor
+  {
 
     /** {@inheritDoc} */
     @Override
-    public String getDisplayName() {
+    public String getDisplayName()
+    {
       return Messages.DisplayName();
     }
 
     @Override
-    public String getFunctionName() {
+    public String getFunctionName()
+    {
       return "logstashSend";
     }
 
     @Override
-    public Set<? extends Class<?>> getRequiredContext() {
+    public Set<? extends Class<?>> getRequiredContext()
+    {
       Set<Class<?>> contexts = new HashSet<>();
       contexts.add(TaskListener.class);
       contexts.add(Run.class);
