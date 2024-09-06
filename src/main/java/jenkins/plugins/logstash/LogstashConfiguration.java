72c163a30c0fb2a4d296068afd1c0846a1f72def
Shreyas Zanpure <shreyas.zanpure@gmail.com>
2024-09-06 12:30:46 +0100

    Added import for jenkins.util.JenkinsJVM

--- src/main/java/jenkins/plugins/logstash/LogstashConfiguration.java
+++ src/main/java/jenkins/plugins/logstash/LogstashConfiguration.java
@@ -28,6 +28,7 @@ import jenkins.plugins.logstash.configuration.Redis;
 import jenkins.plugins.logstash.configuration.Syslog;
 import jenkins.plugins.logstash.persistence.LogstashIndexerDao;
 import jenkins.plugins.logstash.persistence.LogstashIndexerDao.IndexerType;
+import jenkins.util.JenkinsJVM
 import net.sf.json.JSONObject;
 
 @Extension
