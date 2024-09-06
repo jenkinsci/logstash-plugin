3af23c4c439befb4a56cdf7d545e3d96699838ce
Shreyas Zanpure <shreyas.zanpure@gmail.com>
2024-09-06 12:16:17 +0100

    Update LogstashWriter.java

--- src/main/java/jenkins/plugins/logstash/LogstashWriter.java
+++ src/main/java/jenkins/plugins/logstash/LogstashWriter.java
@@ -159,6 +159,10 @@ public class LogstashWriter implements Serializable {
 
   // Method to encapsulate calls for unit-testing
   LogstashIndexerDao getIndexerDao() {
+    if (!JenkinsJVM.isJenkinsJVM()){
+      return null;
+    }
+    
     return LogstashConfiguration.getInstance().getIndexerInstance();
   }
 
